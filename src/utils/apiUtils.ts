0417d6b3457ef8391256b142adfc105c3806e64b
Koonosuke <kou@cps.im.dendai.ac.jp>
2025-04-01 23:28:44 +0900

    a

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -9,7 +9,7 @@ import {
   UserResponse,
 } from "@/types/index";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // 共通の fetch ラッパー
 const apiRequest = async <T>(
